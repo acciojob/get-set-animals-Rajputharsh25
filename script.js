946d08428818f72f531e604f4d8de3db7614d562
Rajputharsh25 <144730809+Rajputharsh25@users.noreply.github.com>
2026-02-20 19:04:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,55 @@
-//complete this code
-class Animal {}
+// Animal Class
+class Animal {
+    constructor(species) {
+        this._species = species;
+    }
 
-class Dog extends Animal {}
+    // Getter for species
+    get species() {
+        return this._species;
+    }
 
-class Cat extends Animal {}
+    // Method to make sound
+    makeSound() {
+        console.log(`The ${this.species} makes a sound`);
+    }
+}
 
-// Do not change the code below this line
-window.Animal = Animal;
-window.Dog = Dog;
-window.Cat = Cat;
+
+// Cat Class (inherits Animal)
+class Cat extends Animal {
+    constructor(species) {
+        super(species);
+    }
+
+    // Method for purring
+    purr() {
+        console.log("purr");
+    }
+}
+
+
+// Dog Class (inherits Animal)
+class Dog extends Animal {
+    constructor(species) {
+        super(species);
+    }
+
+    // Method for barking
+    bark() {
+        console.log("woof");
+    }
+}
+
+
+// Example usage
+
+// Cat example
+const myCat = new Cat("Siamese");
+myCat.makeSound(); // The Siamese makes a sound
+myCat.purr();      // purr
+
+// Dog example
+const myDog = new Dog("Golden Retriever");
+myDog.makeSound(); // The Golden Retriever makes a sound
+myDog.bark();      // woof
